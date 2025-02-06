00c763faa2d02a60a16f82d5394621e627d826c7
Chaimaekit <chaimaeeljerrar11@gmail.com>
2025-02-06 01:45:18 +0100

    4th commit that creates routes

--- app/main.py
+++ app/main.py
@@ -1,25 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
-from  motor.motor_asyncio import AsyncIOMotorClient
+from routes import router
+from bson import ObjectId
 
 app = FastAPI()
 
-client = AsyncIOMotorClient("mongodb://localhost:27017")
-db = client["mynddatabase"]
-collection = db["tenders"]
+app.include_router(router)
 
-def id_to_string(mongo_obj):
-    mongo_obj['_id'] = str(mongo_obj['_id'])
-    return mongo_obj
-
-@app.get("/")
-async def home_page():
-    return {"Hello again with no knowledge "}
-
-@app.get("/all/objects")
-async def show_all():
-    items= await collection.find().to_list(None)
-    return {"items": items} 
 
 if __name__== "__main__":
     uvicorn.run(app, host="127.0.0.1",port=8080)
---
+++ app/routes.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from  motor.motor_asyncio import AsyncIOMotorClient
+
+router=APIRouter()
+
+client = AsyncIOMotorClient("mongodb://localhost:27017")
+db = client["mynddatabase"]
+collection = db["tenders"]
+
+
+def id_to_string(mongo_objs):
+    for mongo_obj in mongo_objs:
+     mongo_obj['_id'] = str(mongo_obj['_id'])
+    return mongo_objs
+
+@router.get("/")
+async def home_page():
+    return {"Hello again with no knowledge "}
+
+@router.get("/all/objects")
+async def show_all():
+    items= await collection.find().to_list(None)
+    return {"items": id_to_string(items)} 
+
+
+@router.delete("/delete/{obj_id}")
+async def delete_obj(obj_id: str):
+   delete_obj=await collection.delete_one({"_id":ObjectId(obj_id)})
+   if delete_obj.deleted_count==1:
+      return {"result": "the object got deleted successfully"}
+   else:
+      return {"result":"fatal error #####"}
